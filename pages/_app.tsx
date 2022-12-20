4934d064ecda0a7193ed27ac475e9701efd7552c
Nkiriobasi <obasinkiri@gmail.com>
2022-12-20 17:57:16 +0100

    added Layout exception for sign-in page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,10 +6,16 @@ import {useRouter} from 'next/router';
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
-  if(router.asPath =='/sign-up')  {
-     return (
-       <Component {...pageProps} />
-     )
+  if(router.asPath =='/sign-up') {
+    return (
+      <Component {...pageProps} />
+    )
+  }
+
+  if(router.asPath =='/sign-in') {
+    return (
+      <Component {...pageProps} />
+    )
   }
 
   return (
