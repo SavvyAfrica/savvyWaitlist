e4ed33a258c7cbd9668c2363ad5172f8982ef5e2
Nkiriobasi <obasinkiri@gmail.com>
2022-12-20 14:07:06 +0100

    used useRouter to exempt the sign-up page from the Layout component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import type { AppProps } from "next/app";
 import Layout from "../layout/layout";
 import "../styles/globals.css";
+import {useRouter} from 'next/router';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  if(router.asPath =='/sign-up')  {
+     return (
+       <Component {...pageProps} />
+     )
+  }
+
   return (
     <Layout>
       <Component {...pageProps} />
