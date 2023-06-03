6e0be3760386a12b9b24c97ab44979f40f538ece
kedehboy <ayangcostly@gmail.com>
2023-06-03 11:41:38 +0100

    Pushing update fixes

--- features/hompage/frame1/frame.tsx
+++ features/hompage/frame1/frame.tsx
@@ -4,14 +4,14 @@ import { Text } from '../../../components/typography/typography'
 
 function Frame() {
   return (
-    <div className='maxWidth mx-auto w-full flex flex-col justify-center items-center py-5 px-5 lg:pt-5 lg:pb-5 lg:pl-10 lg:pr-10 md:mb-36 mb-20'>
+    <div className='flex flex-col items-center justify-center w-full px-5 py-5 mx-auto mb-20 maxWidth lg:pt-5 lg:pb-5 lg:pl-10 lg:pr-10 md:mb-36'>
       <div
         className={`frame rounded-[10px] xl:w-[52%] lg:w-[70%] 
         md:w-[80%] w-full sm:h-[98px] h-auto flex justify-center items-center`}
       >
         <Text
           variant='home_heading_3_32px'
-          className='sm:text-[32px] text-[24px] text-center sm:px-0 px-10 sm:py-0 py-3 font-semibold text-[#292D32] mx-auto'
+          className='text-[19px] lg:text-[24px] text-center sm:px-0 px-10 sm:py-0 py-3 font-semibold text-[#292D32] mx-auto'
         >
           Save Beyond Your Expectations
         </Text>
