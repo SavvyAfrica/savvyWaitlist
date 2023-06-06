f632f1d9414916d41f52cfb2bda6db746f471570
kedehboy <ayangcostly@gmail.com>
2023-06-06 22:02:28 +0100

    Done with the SEO and also favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,12 +5,26 @@ import '../styles/globals.css'
 // import { globalState } from '../global/store';
 import 'react-tooltip/dist/react-tooltip.css'
 import { Toaster } from 'react-hot-toast'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   // const {showNav, setShowNav} = globalState();
 
   return (
     <Layout>
+      <Head>
+        <title>Gadget Subscription For Africans</title>
+        <meta
+          name='description'
+          content='Buy or rent the gadgets you need, when you need them - the flexible and easy way with Savvy'
+        />
+        <meta
+          name='keywords'
+          content='African gadget subscription service, Budget-friendly tech gadgets, Affordable gadget leasing,Subscription-based gadgets, Wallet-friendly tech in Africa'
+        />
+        <meta name='author' content='Your Name' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      </Head>
       <Component {...pageProps} />
       <Toaster position='top-right' reverseOrder={false} />
     </Layout>
